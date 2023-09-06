78f2f8d1b9db6240520e183920b8ba6372ba70f2
IgorMushk <igor.mushketik@gmail.com>
2023-09-06 17:48:54 +0300

    <Container> styled.div

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
+import { Container } from "./App.styled";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      {/* React homework template */}
+    </Container>
   );
 };
+
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
---
+++ src/components/App.styled.jsx
@@ -0,0 +1,24 @@
+import { styled } from 'styled-components';
+
+export const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 40px;
+  color: #010101;
+`;
+
+// export const Container = styled.div`
+//   //height: 100vh;
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+//   align-items: center;
+//   font-size: 40px;
+//   color: #010101;
+//   max-width: 1400px;
+//   padding: 20px 20px;
+//   margin: 0 auto;
+//   gap: 30px;
+// `;
