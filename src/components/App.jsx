d404aa1f0100503dc47123206ec2419e70a4ac9f
IgorMushk <igor.mushketik@gmail.com>
2023-09-06 18:16:31 +0300

    App to class

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,20 @@
-import { Container } from "./App.styled";
+import { Component } from 'react';
+import { Container } from './App.styled';
 
-export const App = () => {
-  return (
-    <Container>
-      {/* React homework template */}
-    </Container>
-  );
-};
+export class App extends Component {
+  render() {
+    return <Container>React homework template</Container>;
+  }
+}
 
+
+// export const App = () => {
+//   return (
+//     <Container>
+//       {/* React homework template */}
+//     </Container>
+//   );
+// };
 
 // export const App = () => {
 //   return (
