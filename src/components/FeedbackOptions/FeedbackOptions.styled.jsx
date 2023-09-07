b5f6d9ce2bdc5634d1aa404f6a514959aa11da62
IgorMushk <igor.mushketik@gmail.com>
2023-09-07 21:20:46 +0300

    upd styled for buttons

--- src/components/FeedbackOptions/FeedbackOptions.styled.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -12,33 +12,42 @@ export const ButtonStyle = styled.button`
   width: 75px;
   height: 30px;
   text-transform: capitalize;
-  
+  transition: linear 300ms;
+
   &.good {
-   border: 1px solid #24b81f;
-    }
+    border: 1px solid #24b81f;
+  }
   &.good:hover {
-   background-color: #24b81f;
-   color: white;
-   cursor: pointer;
+    background-color: #24b81f;
+    color: white;
+    cursor: pointer;
+  }
+  &.good:active {
+    scale: 0.85;
   }
 
   &.neutral {
-   border: 1px solid #c7b406;
-    }
+    border: 1px solid #c7b406;
+  }
   &.neutral:hover {
-   background-color: #c7b406;
-   color: white;
-   cursor: pointer;
+    background-color: #c7b406;
+    color: white;
+    cursor: pointer;
+  }
+  &.neutral:active {
+    scale: 0.85;
   }
 
   &.bad {
-   border: 1px solid #ec5252;
-   //border: '1px solid red'
+    border: 1px solid #ec5252;
+    //border: '1px solid red'
   }
   &.bad:hover {
-   background-color: #ec5252;
-   color: white;
-   cursor: pointer;
+    background-color: #ec5252;
+    color: white;
+    cursor: pointer;
   }
-
+  &.bad:active {
+    scale: 0.85;
+  }
 `;
